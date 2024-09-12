14bdf6d8c99b77da725322a1e843a977e75ce290
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-12 21:47:50 +0700

    2.5. Lấy ra chủ đề bài hát

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ dotenv.config();
 
 import { connectDatabase } from "./config/database";
 
+import Topic from "./models/topic.model";
+
 connectDatabase();
 
 const app: Express = express();
@@ -12,7 +14,13 @@ const port: number | string = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/topics", (req: Request, res: Response) => {
+app.get("/topics", async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false
+  });
+
+  console.log(topics);
+
   res.render("client/pages/topics/index");
 });
 
