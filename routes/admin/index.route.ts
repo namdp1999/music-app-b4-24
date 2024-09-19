9636eb107f79b58a6beb2bff10d9302539190d23
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-19 21:45:34 +0700

    4. Tạo trang Danh sách bài hát (bên admin)

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 import { dashboardRoute } from "./dashboard.route";
 import { systemConfig } from "../../config/system";
 import { topicRoute } from "./topic.route";
+import { songRoute } from "./song.route";
 
 export const routesAdmin = (app: Express) => {
   const PATH = `/${systemConfig.prefixAdmin}`;
@@ -9,4 +10,6 @@ export const routesAdmin = (app: Express) => {
   app.use(`${PATH}/dashboard`, dashboardRoute);
 
   app.use(`${PATH}/topics`, topicRoute);
+
+  app.use(`${PATH}/songs`, songRoute);
 }
