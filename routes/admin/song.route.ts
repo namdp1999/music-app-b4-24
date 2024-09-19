ecfcf5e4eeb5e4b561677cc10a4a323d88017229
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-19 22:40:12 +0700

    5.2. Server lấy data và lưu vào database

--- routes/admin/song.route.ts
+++ routes/admin/song.route.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import multer from "multer";
 const router = express.Router();
 
 import * as controller from "../../controllers/admin/song.controller";
+import * as uploadCoud from "../../middlewares/admin/uploadCloud.middleware";
+
+const upload = multer();
 
 router.get("/", controller.index);
 
 router.get("/create", controller.create);
+
+router.post(
+  "/create",
+  upload.single("avatar"),
+  uploadCoud.uploadSingle,
+  controller.createPost
+);
 
 export const songRoute = router;
